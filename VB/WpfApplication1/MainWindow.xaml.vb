cc6af4908fa4f56912a2f9874ad88990b6674f55
DevExpressExampleBot <fakeEmail>
2022-10-04 18:22:07 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -1,27 +1,28 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 
 Namespace WpfApplication1
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = Stuff.GetStuff()
-		End Sub
 
-		Private Sub SmartExpandNodes(ByVal minChildCount As Integer)
-			Dim nodeIterator As New TreeListNodeIterator(treeListView.Nodes, True)
-			Do While nodeIterator.MoveNext()
-				nodeIterator.Current.IsExpanded = nodeIterator.Current.Nodes.Count >= minChildCount
-			Loop
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub grid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			SmartExpandNodes(4)
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = GetStuff()
+        End Sub
+
+        Private Sub SmartExpandNodes(ByVal minChildCount As Integer)
+            Dim nodeIterator As TreeListNodeIterator = New TreeListNodeIterator(Me.treeListView.Nodes, True)
+            While nodeIterator.MoveNext()
+                nodeIterator.Current.IsExpanded = nodeIterator.Current.Nodes.Count >= minChildCount
+            End While
+        End Sub
+
+        Private Sub grid_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            SmartExpandNodes(4)
+        End Sub
+    End Class
 End Namespace
